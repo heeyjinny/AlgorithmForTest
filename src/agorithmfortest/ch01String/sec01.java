4a1af1dabeef78ff65fa2b8bb116321b55029f04
heeyjinny <heeyjinny@gmail.com>
2023-02-01 22:34:14 +0900

    알고리즘 문자찾기

---
+++ src/agorithmfortest/ch01String/sec01.java
@@ -0,0 +1,32 @@
+package agorithmfortest.ch01String;
+
+import java.util.*;
+
+public class sec01 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
+
+
+/**  문자 찾기  **/
+
+class Main {
+	public static int solution(String str, char t) {
+		int answer = 0;
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println();
+		
+	}
+	
+}
