379fb7ef779fc007da8c33776a3ce5fb7eb84f27
heeyjinny <heeyjinny@gmail.com>
2023-02-07 23:31:45 +0900

    대소문자 변환

--- src/algorithmfortest/ch02String/sec02.java
+++ src/algorithmfortest/ch02String/sec02.java
@@ -2,17 +2,9 @@ package algorithmfortest.ch02String;
 
 import java.util.*;
 
-public class sec02 {
-
-	public static void main(String[] args) {
-
-	}
-
-}
-
 /** 대소문자 변환  **/
 
-class Main{
+public class sec02 {
 	
 	public static String solution(String str) {
 		String answer = "";
@@ -20,7 +12,7 @@ class Main{
 		//배열요소를 하나하나 x에대입
 		for(char x : str.toCharArray()) {
 			
-			//Character클래스 사용하여 대문자나 소문자 탐
+			//Character클래스 사용하여 대문자나 소문자 탐색
 			if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
 			else answer += Character.toLowerCase(x);
 		}
@@ -36,4 +28,5 @@ class Main{
 		
 		System.out.println(solution(str));
 	}
+
 }
